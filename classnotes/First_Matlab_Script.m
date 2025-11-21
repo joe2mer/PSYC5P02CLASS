e1de271f5422c0c8984159a12abd41325f6ff442
joe2mer <joe2mer@gmail.com>
2025-11-20 22:08:43 -0500

    adding new notes for matlab

---
+++ classnotes/First_Matlab_Script.m
@@ -0,0 +1,80 @@
+% Matlab uses the end keyword to terminate blocks of code 
+% multi-line is %{...%}
+
+% Defining vectors
+
+% defined within square brackets 
+
+a = [1,2,3,4,5]
+
+% omit the semi colon and it prints the output to the terminal 
+%clc clears the command window
+%clear clears everything 
+
+1+1
+
+% if you do not assign a name to the variable it will create a variable
+% called ans 
+
+%% 
+
+%creates a section 
+
+%%
+
+% if you type the name of the file into the command window it will execute
+% all of the code
+
+% who tells us who exists in our variables and whos provides more
+% information 
+
+% can clear specific variables from your work space via clear and the name 
+
+% semi colons denote the columns 
+
+% matrices must be rectangular. Must have an element at every location in a
+% rectangular matrix 
+
+% strings are in quotes 
+
+% indexing is done with the round brackets 
+
+% m(3,1) indexing row by column 
+
+% m([1 3],1) taking the first and third rows from the first column 
+
+% colon says give me everything 
+
+% m(2:3,2) specifies a range 
+
+% a = 1:2:10 first specifies the starting point, 2 specifies the step, and
+% 10 specifies the end 
+
+%' does a matrix transpose. takes the row and column vectors and switches
+%them 
+
+% can concatenate by adding two variables in square brackets 
+
+% indexing a non-existent value in your matrix will extend your matrix 
+
+%  c(6:9) = [] says to delete these values 
+
+% zeroes, ones, and NaN, specify the dimensions of an array 
+
+% flip will flip a matrix keeping order intact 
+
+% multplication and division perform matrix multiplication and division 
+
+% you can save data into mat files using the save command 
+
+% help functions are found using help and then the name of the function 
+
+% mean(a) will calculate the mean column wise 
+
+% to calculate it by row transpose the variable 
+
+% mean(a, 'all') will do it over the entire matrix 
+
+% can reset the rng using rng('shuffle')
+
+% matlab has imaginary numbers 
